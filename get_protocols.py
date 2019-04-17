506a655a5226ed3a0b9975bfc57371ce6e1ebe27
brunobelloni <bruno.belloni@hotmail.com>
2019-04-16 23:26:16 -0300

    inicio do cgi com shell script

--- get_protocols.py
+++ get_protocols.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-#!/usr/bin/python3
-
 import psycopg2
 from config import config
 
---
+++ teste.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+echo "Content-type: text/html\n\n";
+echo "<html>"
+echo "<head>"
+echo "</head>"
+echo "<body>"
+echo "Hello World"
+echo "</body>"
+echo "</html>"
